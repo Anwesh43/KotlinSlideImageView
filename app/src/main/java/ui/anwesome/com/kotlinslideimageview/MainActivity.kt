2635aa5d72c40ef547ec23d767c24b72d0e233af
Anwesh43 <anweshthecool0@gmail.com>
2018-01-26 02:48:07 +0530

    using SlideImageView in activity

--- app/src/main/java/ui/anwesome/com/kotlinslideimageview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinslideimageview/MainActivity.kt
@@ -1,12 +1,14 @@
 package ui.anwesome.com.kotlinslideimageview
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.slideimageview.SlideImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SlideImageView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature_more))
     }
 }
