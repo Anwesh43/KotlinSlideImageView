e209ed2685bcd50d73a54d7afa052b136bbbd659
Anwesh43 <anweshthecool0@gmail.com>
2018-01-26 01:49:12 +0530

    adding SlideImageView class

--- slideimageview/src/main/java/ui/anwesome/com/slideimageview/SlideImageView.kt
+++ slideimageview/src/main/java/ui/anwesome/com/slideimageview/SlideImageView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.slideimageview
 /**
  * Created by anweshmishra on 26/01/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class SlideImageView(ctx:Context,var bitmap:Bitmap):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
