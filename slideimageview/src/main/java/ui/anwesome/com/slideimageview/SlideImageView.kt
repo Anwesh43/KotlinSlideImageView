51e09facb6ae0cbf66db71e92cfe457b87421399
Anwesh43 <anweshthecool0@gmail.com>
2018-01-26 02:10:08 +0530

    adding SlideImageState

--- slideimageview/src/main/java/ui/anwesome/com/slideimageview/SlideImageView.kt
+++ slideimageview/src/main/java/ui/anwesome/com/slideimageview/SlideImageView.kt
@@ -49,4 +49,21 @@ class SlideImageView(ctx:Context,var bitmap:Bitmap):View(ctx) {
 
         }
     }
+    data class SlideImageState(var scale:Float = 0f,var dir:Float = 0f,var prevScale:Float = 0f) {
+        fun update(stopcb:(Float)->Unit) {
+            scale += 0.1f*dir
+            if(Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb:()->Unit) {
+            if(dir == 0f) {
+                dir = 1-2*scale
+                startcb()
+            }
+        }
+    }
 }
