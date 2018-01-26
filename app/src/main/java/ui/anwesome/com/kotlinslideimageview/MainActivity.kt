ab2196980f9b6619d3c004717b02a0a6689b9edb
Anwesh43 <anweshthecool0@gmail.com>
2018-01-26 14:40:12 +0530

    using SlideImageListener in app

--- app/src/main/java/ui/anwesome/com/kotlinslideimageview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinslideimageview/MainActivity.kt
@@ -3,12 +3,18 @@ package ui.anwesome.com.kotlinslideimageview
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.slideimageview.SlideImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SlideImageView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature_more))
+        val view = SlideImageView.create(this,BitmapFactory.decodeResource(resources,R.drawable.nature_more))
+        view.addSlideImageListener({
+            Toast.makeText(this,"it's on",Toast.LENGTH_SHORT).show()
+        },{
+            Toast.makeText(this,"it's off",Toast.LENGTH_SHORT).show()
+        })
     }
 }
